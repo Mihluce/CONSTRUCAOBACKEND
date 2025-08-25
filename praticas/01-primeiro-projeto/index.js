445c07d11abf1b032f63322d54c3f8ffc3143b08
Mihluce <mirela.lucena28@gmail.com>
2025-08-25 20:01:48 -0300

    primeiro commit

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,19 @@
+console.log("Meu primeiro projeto!!!")
+
+
+console.log("hello")
+
+let nome = 'Mirela'
+
+let sobrenome = 'Lucena'
+
+let idade = 19
+
+console.log("olá" + nome + "" + sobrenome)
+comsole.log("Sua idade é " + idade)
+
+if(idade >= 18){
+    console.log("Voce é maior de idade!!!")
+}else{
+    console.log("Voce é menor de idade")
+}
